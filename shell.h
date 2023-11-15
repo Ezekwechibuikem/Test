a495cf43a92e389d67d20879505b94cf21ac568e
Evar12 <Evaristus12@gmail.com>
2023-11-15 14:23:28 -0800

    Evar

--- shell.h
+++ shell.h
@@ -1,68 +1,57 @@
-#include "shell.h"
+#ifndef SHELL_H
+#define SHELL_H
 
-int exitcode = 0;
-int errorcount = 0;
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-/**
- * main - a simple shell program written in C
- * @argc: number of arguments
- * @argv: array of arguments
- * @env: array of environment variables
- *
- * Return: 0 always (but program may exit early)
- */
+extern int exitcode;
+extern int errorcount;
 
-int main(__attribute__((unused)) int argc, char **argv, char **env)
-{
-	char *user_input = NULL;
-	char **commands = NULL;
-	char **path_array = NULL;
-	size_t nbytes = 0;
-	ssize_t bytes_read = 0;
-	char *NAME = argv[0];
-	int atty_is = isatty(0);
-	char *filename = "splash_screen.txt";
-	FILE *fptr = NULL;
+/* check_helpers */
+int exit_check(char *user_input, char *NAME);
+int blank_check(char *user_input);
+int path_check(char *command);
+int env_check(char *user_input);
 
-	signal(SIGINT, SIG_IGN);
+/* error_helpers */
+void command_error(char *NAME, char *command);
+void exec_error(char *NAME, char *command);
+void access_error(char *NAME, char *command);
+void exit_error(char *NAME, char *user_input);
 
-	// display splash screen
-	if((fptr = fopen(filename,"r")) == NULL)
-	{
-		fprintf(stderr,"error opening %s\n",filename);
-		return 1;
-	}
-	display_splash_screen(fptr);
-	fclose(fptr);
+/* fork_wait_exec */
+void fork_wait_exec(char **commands, char **path_array,
+		    char **env, char *NAME, char *user_input);
 
-	while (1)
-	{
-		errorcount++;
-		if (atty_is)
-			write(STDOUT_FILENO, "hella_shell$ ", 13);
-		bytes_read = getline(&user_input, &nbytes, stdin);
-		if (bytes_read == -1)
-		{
-			free(user_input);
-			exit(exitcode);
-		}
-		if (exit_check(user_input, NAME) == -1)
-			continue;
-		if (blank_check(user_input) == 1)
-			continue;
-		if (env_check(user_input) == 1)
-		{
-			print_env(env);
-			continue;
-		}
-		path_array = get_path_array(env);
-		commands = parse_input(user_input, path_array, NAME);
-		if (commands != NULL)
-		{
-			fork_wait_exec(commands, path_array, env, NAME, user_input);
-			free_array(commands);
-			free_array(path_array);
-		}
-	}
-	return (0);
-}
+/* memory_helpers */
+void free_array(char **array);
+
+/* number_helpers */
+int _atoi(char *s);
+void print_number(int n);
+
+/* parse_input */
+int arg_counter(char *user_input);
+char **parse_input(char *user_input, char **path_array, char *NAME);
+
+/* string_helpers */
+int _strlen(char *str);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
+int _putchar(char c);
+
+/* env_helpers */
+int get_path_count(char *path);
+char **get_path_array(char **env);
+char *find_path(char **path_array, char *token);
+void print_env(char **env);
+
+/* splash screen */
+void display_splash_screen(FILE *file_ptr);
+
+#endif
